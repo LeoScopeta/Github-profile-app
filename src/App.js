b71683c649af56a50dd938ddda53a66a7fd13cbe
LeoScopeta <65691356+LeoScopeta@users.noreply.github.com>
2022-01-28 16:22:39 -0300

    App finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Layout from "./components/layout";
+import NoSearch from "./components/no-search";
+import Profile from "./components/profile";
+import Repositories from "./components/repositories";
+import useGithub from "./hooks/github-hooks";
 
-function App() {
+const App = () => {
+  const { githubState } = useGithub();
   return (
-    <div>
-      
-    </div>
+    <Layout>
+      {githubState.hasUser ? (
+        <>
+          {githubState.loading ? (
+            <p>Loading</p>
+          ) : (
+            <>
+              <Profile />
+              <Repositories />
+            </>
+          )}
+        </>
+      ) : (
+        <NoSearch />
+      )}
+    </Layout>
   );
-}
+};
 
 export default App;
